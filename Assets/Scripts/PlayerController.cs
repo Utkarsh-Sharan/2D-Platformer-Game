9d299024ea8358a9e4d5e8088883b5010ea78ce5
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-16 12:09:04 +0530

    implemented jump and crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,70 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
 
+    private Rigidbody2D _rigidBody;
+    private bool _isGrounded = true;
+    private float _jumpForce = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
         float speed = Input.GetAxisRaw("Horizontal");
         _anim.SetFloat("Speed", Mathf.Abs(speed));
         Vector2 _scale = transform.localScale;
 
-        if(speed < 0)
+        if (speed < 0)
         {
             _scale.x = -1.0f * Mathf.Abs(_scale.x);
         }
-        else if(speed > 0)
+        else if (speed > 0)
         {
             _scale.x = Mathf.Abs(_scale.x);
         }
 
         transform.localScale = _scale;
+    }
+
+    private void Crouch()
+    {
+        _anim.SetBool("Crouch", true);
+    }
+
+    private void Jump()
+    {
+        _isGrounded = false;
+
+        _anim.SetBool("Jump", true);
+
+        _rigidBody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            _isGrounded = true;
+            _anim.SetBool("Jump", false);
+        }
     }
 }
