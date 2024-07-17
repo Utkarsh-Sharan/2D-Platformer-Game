38a82170cdba5eb1b3f4a5854d6ca0c265ec1831
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-17 13:14:56 +0530

    implemented key animation

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    private Animator _keyAnimator;
+
+    private void Awake()
+    {
+        _keyAnimator = GetComponent<Animator>(); 
+        if(_keyAnimator == null)
+        {
+            Debug.LogError("Key animator null!");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
             PlayerController playerController = GameObject.Find("Player").GetComponent<PlayerController>();
             playerController.PickUpKey();
+
+            StartCoroutine(KeyCollectedAnimationTimer());
             Destroy(this.gameObject);
         }
+    }
+
+    IEnumerator KeyCollectedAnimationTimer()
+    {
+        _keyAnimator.SetBool("Collected", true);
+        yield return new WaitForSeconds(2f);
     }
 }
