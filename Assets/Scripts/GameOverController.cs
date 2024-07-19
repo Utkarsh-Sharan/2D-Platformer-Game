7d314a9abff16fe0fd666f2e9c14a3a0ac9b550c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-19 16:30:12 +0530

    added sound system

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -7,11 +7,15 @@ public class GameOverController : MonoBehaviour
 {
     public void RestartGame()
     {
+        SoundManager.Instance.Play(Sounds.BUTTON_CLICK);
+        SoundManager.Instance.PlayMusic(Sounds.MUSIC);
         SceneManager.LoadScene(1);
     }
 
     public void BackToMenu()
     {
+        SoundManager.Instance.Play(Sounds.BUTTON_CLICK);
+        SoundManager.Instance.PlayMusic(Sounds.MUSIC);
         SceneManager.LoadScene(0);
     }
 }
